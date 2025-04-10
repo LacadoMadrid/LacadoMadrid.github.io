7e6ad2fa7d9299819c1fb6c2bbe0eea192d39302
LacadoMadrid <161258246+LacadoMadrid@users.noreply.github.com>
2025-04-10 23:12:54 +0200

    script.js

--- script.js
+++ script.js
@@ -63,12 +63,3 @@ function confirmCall() {
         window.location.href = "tel:+34627560307";
     }
 }
-
-
-  const burger = document.getElementById('burger');
-  const navLinks = document.getElementById('nav-links');
-
-  burger.addEventListener('click', () => {
-    navLinks.classList.toggle('show');
-  });
-
